00d375b48c946c772bd6421bf56ad6c196ecd496
Jiří Hermann <jiri.hermann@gendigital.com>
2025-10-12 22:48:02 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -18,16 +18,16 @@ export default defineConfig({
     eslintPlugin(),
   ],
   server: {
-    port: 3333,
+    port: 3000,
     proxy: {
       '/actuator': {
         target:
-          process.env.VITE_PROXY_TARGET ?? 'http://localhost:8080',
+          process.env.VITE_PROXY_TARGET ?? 'http://localhost:8891',
         changeOrigin: true,
       },
       '/api': {
         target:
-          process.env.VITE_PROXY_TARGET ?? 'http://localhost:8080',
+          process.env.VITE_PROXY_TARGET ?? 'http://localhost:8891',
         changeOrigin: true,
       },
       // BFF session & SSE endpoints (proxy to BE so we avoid CORS and keep HttpOnly cookies)
